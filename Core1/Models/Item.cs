a412b2d5730a1b8d31943654ee34ace4d9823465
Muhammad Eko Jumaddin <ekojumaddin@gmail.com>
2018-12-12 09:54:14 +0800

    model item by eko

---
+++ Core1/Models/Item.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core1.Models
+{
+    public class Item : BaseModel
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Stock { get; set; }
+        public string Image { get; set; }
+        [ForeignKey("Supplier")] 
+        public int SupplierId { get; set; }
+        public Supplier Supplier { get; set; }
+    }
+}
